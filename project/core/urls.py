15fdca2e3cf2da61722f1d13b0d1c4cfb102bf35
Marcos Paz <m.paz92@hotmail.com>
2024-05-23 15:29:54 -0300

    agrego pagina about

--- project/core/urls.py
+++ project/core/urls.py
@@ -7,6 +7,7 @@ app_name = "core"
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("about/", views.about, name="about"),
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
--- project/core/views.py
+++ project/core/views.py
@@ -10,6 +10,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, "core/index.html")
 
+def about(request):
+    return render(request, "core/about.html")
 
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
